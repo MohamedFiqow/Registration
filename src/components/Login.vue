4dbfbb82bc74d981ecd23459f0a8ca56dfdd4506
fiqow <54118455+MohamedFiqow@users.noreply.github.com>
2023-07-31 14:38:31 +0300

    aaa

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="container">
+<form action="#" class="form">
+    <div class="input-box">
+      <label>Full Name</label>
+      <input type="text" placeholder="Enter full name" required />
+    </div>
+
+    <div class="input-box">
+      <label>Email Address</label>
+      <input type="text" placeholder="Enter email address" required />
+    </div>
+</form>
+</section>
+</template>
